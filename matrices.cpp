7bbb2b2abeae6b962ebac7558f7aad531ed70550
Alex Doehring <129962613+AlexDoehring@users.noreply.github.com>
2023-10-18 15:14:32 -0500

    finished commenting matrices.cpp

--- matrices.cpp
+++ matrices.cpp
@@ -16,25 +16,30 @@ void subMatrix(int mat1[][MAX_SIZE], int mat2[][MAX_SIZE], int ansMat[][MAX_SIZE
 
 //Main code
 int main() {
+    //initializes file name and each matrix
     string filename = "matrix_input.txt";
     int matrix1[MAX_SIZE][MAX_SIZE];
     int matrix2[MAX_SIZE][MAX_SIZE];
-    int size = readFile(filename, matrix1, matrix2);
-    
+    int size = readFile(filename, matrix1, matrix2); //initializes size and two matrices
+
+    //prints each matrix
     cout << "Matrix A:" << endl;
     printMatrix(matrix1, size);
     cout << "Matrix B:" << endl;
     printMatrix(matrix2, size);
 
+    //Adds the two matrices and prints the result
     int ansMat[MAX_SIZE][MAX_SIZE];
     addMatrix(matrix1, matrix2, ansMat, size);
     cout << "A + B = " << endl;
     printMatrix(ansMat, size);
 
+    //Multiplies the two matrices and prints the result
     multMatrix(matrix1, matrix2, ansMat, size);
     cout << "A * B = " << endl;
     printMatrix(ansMat, size);
 
+    //Subtracts the two matrices and prints the result
     subMatrix(matrix1, matrix2, ansMat, size);
     cout << "A - B = " << endl;
     printMatrix(ansMat, size);
@@ -52,57 +57,62 @@ int readFile(string fileName, int matrix1[][MAX_SIZE], int matrix2[][MAX_SIZE]) 
     // loops through the file for the first matrix and initializes it
     for(int i = 0; i < size; i++) {
         getline(file, line);
-        stringstream s(line); //initializes string stream variable which makes splitting the string up easier
+        stringstream s(line); //initializes stringstream variable which makes splitting the string up easier
         for(int j = 0; j < size; j++) {
-            s >> matrix1[i][j]; //takes each word from the stringstream and sets each matrix value to the respective word
+            s >> matrix1[i][j]; //takes each word from the stringstream and sets each matrix value to the respective int
         }
     }
 
+    // loops through the file for the second matrix and initializes it
     for(int i = 0; i < size; i++) {
         getline(file, line);
-        stringstream s(line);
+        stringstream s(line); //initializes stringstream variable 
         for(int j = 0; j < size; j++) {
-            s >> matrix2[i][j];
+            s >> matrix2[i][j]; //takes each word from the stringstream and sets each matrix value to the respective int
         }
     }
 
-    file.close();
-    return size;
+    file.close(); //closes file
+    return size; //returns the size
 }
 
+//takes a matrix and its size and prints the matrix with nice format
 void printMatrix(int matrix[][MAX_SIZE], int size) {
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) { //loops through rows and columns
         for (int j = 0; j < size; j++) {
-            cout << matrix[i][j] << ' ';
+            cout << matrix[i][j] << ' '; // prints the matrix element with a space
         }
-        cout << endl;
+        cout << endl; //prints new line for the rows
     }
-    cout << endl;
+    cout << endl; // ends with an extra new line
 }
 
+//takes 2 matrices and adds them into a 3rd matrix
 void addMatrix(int mat1[][MAX_SIZE], int mat2[][MAX_SIZE], int ansMat[][MAX_SIZE], int size) {
-    for(int i = 0; i < size; i++) {
+    for(int i = 0; i < size; i++) { //loops through the matrix
         for(int j = 0; j < size; j++) {
-            ansMat[i][j] = mat1[i][j] + mat2[i][j];
+            ansMat[i][j] = mat1[i][j] + mat2[i][j]; // adds the two matrices and assigns it to the third matrix
         }
     }
 }
 
+//takes 2 matrices and multiplies them into a 3rd matrix
 void multMatrix(int mat1[][MAX_SIZE], int mat2[][MAX_SIZE], int ansMat[][MAX_SIZE], int size) {
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) { //loops through the matrix
         for (int j = 0; j < size; j++) {
             ansMat[i][j] = 0; // Initialize the result matrix element to zero
-            for (int k = 0; k < size; k++) {
-                ansMat[i][j] += mat1[i][k] * mat2[k][j];
+            for (int k = 0; k < size; k++) { //loops again
+                ansMat[i][j] += mat1[i][k] * mat2[k][j]; //computes the product of each element of matrix one and two
             }
         }
     }
 }
 
+//takes 2 matrices and subtracts them into a 3rd matrix
 void subMatrix(int mat1[][MAX_SIZE], int mat2[][MAX_SIZE], int ansMat[][MAX_SIZE], int size) {
-    for(int i = 0; i < size; i++) {
+    for(int i = 0; i < size; i++) { //loops through the matrix
         for(int j = 0; j < size; j++) {
-            ansMat[i][j] = mat1[i][j] - mat2[i][j];
+            ansMat[i][j] = mat1[i][j] - mat2[i][j]; // subtracts the two matrices and assigns it two the third matrix
         }
     }
 }
